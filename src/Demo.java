123a6ef431cbca57e59cfcc8e954589a0852211f
atakan yeşilkayalı <94399983+benevolentsocialplanner@users.noreply.github.com>
2022-07-05 01:16:25 +0300

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,15 +1,5 @@
 
 import javax.swing.DefaultListModel;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author Acer-Notebook
- */
 public class Demo extends javax.swing.JFrame {
         DefaultListModel model;
     /**
